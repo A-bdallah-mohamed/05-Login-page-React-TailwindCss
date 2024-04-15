7d8dd7cc9f975dfbd881d40d8e34647c17d635d7
Abdallah mohamed <162755317+A-bdallah-mohamed@users.noreply.github.com>
2024-04-15 08:28:58 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -24,18 +24,20 @@ function App() {
     
     <div className='h-[100%] w-[100%]'>
     
-    <div className={isitlogin ? `right-0 absolute w-[50%] bg-lime-400 h-[100%] transition-all duration-1000 ease-in-out  overflow-hidden z-40 ` : ' overflow-hidden right-[50%] absolute w-[50%] bg-lime-400 h-[100%] transition-all duration-1000 ease-in-out z-40'} >
-      <div className={isitlogin ? `text-center absolute top-[30%] right-[16.5%] transition-all duration-1000 ease-in-out whitespace-nowrap ` : 'text-center whitespace-nowrap absolute top-[30%] right-[-155%]  transition-all duration-1000 ease-in-out' }>
+    <div className={isitlogin ? `right-0 absolute w-[50%] bg-lime-400 h-[100%] transition-all duration-1000 ease-in-out  overflow-hidden z-40  flex items-center justify-center` : ' overflow-hidden right-[50%] absolute w-[50%] bg-lime-400 h-[100%] transition-all duration-1000 ease-in-out z-40 flex items-center justify-center'} >
+      <div className={isitlogin ? `text-center absolute top-[30%] left-[0] transition-all duration-1000 ease-in-out whitespace-nowrap  w-[100vh]` : 'text-center whitespace-nowrap absolute top-[30%] left-[155%]  transition-all duration-1000 ease-in-out w-[100vh]' }>
         <div className='text-6xl'>Hello, Friend!</div>
         <div><br />To keep connected with us please<br /> enter your personal info</div>
       </div>
-      <div className={isitlogin ? `text-center absolute top-[30%] left-[-150%] transition-all duration-1000 ease-in-out whitespace-nowrap ` : 'text-center whitespace-nowrap absolute top-[30%] left-[19%]  transition-all duration-1000 ease-in-out' }>
+      <div className={isitlogin ? `text-center absolute top-[30%] right-[155%] transition-all duration-1000 ease-in-out whitespace-nowrap  w-[100vh]` : 'text-center whitespace-nowrap absolute top-[30%] right-[0]  transition-all duration-1000 ease-in-out w-[100vh]' }>
       <div className='text-6xl'>Welcome Back! </div>
         <div><br />Enter your personal details and<br />  start your journey with us</div>
       </div>
-      <div onClick={handleclick} className={isitlogin ? `overflow-hidden absolute top-[65%] left-[41%] hover:w-[165px] transition-all duration-1000 ease-in-out whitespace-nowrap bg-lime-400 border-[3px] border-white rounded-2xl w-[155px] h-[40px] hover:h-[50px] hover:bg-lime-200 cursor-pointer` : 'hover:h-[50px] overflow-hidden h-[40px] whitespace-nowrap absolute top-[65%] left-[33%] bg-lime-400 border-[3px] border-white rounded-2xl w-[250px] hover:w-[260px] transition-all duration-1000 ease-in-out hover:bg-lime-200 cursor-pointer' }>
-        <div className={isitlogin ? 'absolute top-[20%] left-[35%] transition-all duration-1000 ease-in-out' : 'top-[20%] left-[100%] absolute opacity-0 transition-all duration-1000 ease-in-out'}>Sign In</div>
-        <div className={isitlogin ? 'top-[20%] right-[100%] absolute opacity-0 transition-all duration-1000 ease-in-out' : 'absolute top-[20%] right-[40%] transition-all duration-1000 ease-in-out'}>Sign Up</div>
+      <div  className={isitlogin ? ` flex items-center justify-center translate-y-[200%]  top-[65%] left-[16.5%] right-[0] transition-all duration-1000 ease-in-out w-[400px] h-[40px]` : 'flex items-center justify-center  transition-all duration-1000 ease-in-out translate-y-[200%] h-[40px]   right-[16.5%] top-[65%]  left-[0] w-[450px]  ' }>
+      <div onClick={handleclick} className={isitlogin ? `overflow-hidden  top-[65%] left-[300px] hover:w-[165px] transition-all duration-1000 ease-in-out whitespace-nowrap bg-lime-400 border-[3px] border-white rounded-2xl w-[155px] h-[40px] hover:h-[50px] hover:bg-lime-200 cursor-pointer` : 'hover:h-[50px] overflow-hidden h-[40px] whitespace-nowrap  top-[65%] left-[33%] bg-lime-400 border-[3px] border-white rounded-2xl w-[250px] hover:w-[260px] transition-all duration-1000 ease-in-out hover:bg-lime-200 cursor-pointer' }>
+        </div>
+        <div className={isitlogin ? 'pointer-events-none absolute top-[20%] left-[43%] transition-all duration-1000 ease-in-out' : 'pointer-events-none top-[20%] left-[100%] absolute opacity-0 transition-all duration-1000 ease-in-out'}>Sign In</div>
+        <div className={isitlogin ? 'pointer-events-none top-[20%] right-[100%] absolute opacity-0 transition-all duration-1000 ease-in-out' : 'pointer-events-none absolute top-[20%] right-[43%] transition-all duration-1000 ease-in-out'}>Sign Up</div>
       </div>
       </div>
     
@@ -46,7 +48,7 @@ function App() {
     <div>
     <div className=' w-[50%] h-[100%] absolute right-0 flex items-center justify-center overflow-hidden '>
     
-    <div className={isitlogin ? ` px-[100px] text-center whitespace-nowrap absolute left-[-50%] transition-all duration-1000 ease-in-out` : ' px-[100px] text-center whitespace-nowrap absolute left-[-13%] transition-all duration-1000 ease-in-out'}>
+    <div className={isitlogin ? ` px-[100px] text-center whitespace-nowrap absolute left-[-50%] transition-all duration-1000 ease-in-out` : ' px-[10px] text-center whitespace-nowrap absolute left-[5px] transition-all duration-1000 ease-in-out'}>
     <div className='text-6xl'>Sign in to StatStation</div>
       <div className='flex items-center justify-center gap-[10px] mt-[25px]'>
     <div className='w-[50px] h-[50px] flex items-center justify-center border-[1px] border-black rounded-full hover:bg-slate-200 cursor-pointer'><FaFacebookF /></div>
